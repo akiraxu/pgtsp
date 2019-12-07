4b000cf99efe690350c0d10becbbe75749b58096
akiraxu <akiraxu@cmail.carleton.ca>
2019-12-06 20:59:29 -0500

    Update pgtsp_mpi.cpp

--- pgtsp_mpi.cpp
+++ pgtsp_mpi.cpp
@@ -306,7 +306,7 @@ int getGlobalMin(int min, int *minPath, int *gMinPath){
         len[i] = paramN + 1;
         offset[i] = i * (paramN + 1);
         for(int j = 0; i < paramN; j++){
-            sendMin[i * (paramN + 1) + j + 1] = minPath[j];
+            //sendMin[i * (paramN + 1) + j + 1] = minPath[j];
         }
     }
 
